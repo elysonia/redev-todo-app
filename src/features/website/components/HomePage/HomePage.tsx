3e97d55bafb12df7276dc95c5f184240057fa286
elysonia <elysoniaalim@gmail.com>
2025-08-05 01:58:50 +0800

    Initial commit

---
+++ src/features/website/components/HomePage/HomePage.tsx
@@ -0,0 +1,21 @@
+import styles from "./homepage.module.css";
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Re:Dev</h1>
+      <h2>Trash isekai-themed website</h2>
+      <p>Maybe this time I'll be a good dev!</p>
+
+      <hgroup>
+        <h3>Projects</h3>
+        <ol>
+          <li>Todo App (the most basic one)</li>
+          <li>Games</li>
+        </ol>
+      </hgroup>
+    </div>
+  );
+};
+
+export default HomePage;
