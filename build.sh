b22b3ac91245cbeeb99cfa24e663787466f6e06d
elysonia <elysoniaalim@gmail.com>
2025-08-14 20:04:07 +0800

    Bash script to only build and deploy when 'main' branch has a new commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+if ["$VERCEL_GIT_COMMIT_REF" = "main"]; then
+    # Only build on "main" branch
+    echo "Build can proceed"
+    exit 1;
+else
+    echo "Build cancelled
+    exit 0
+fi
